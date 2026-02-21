59bcb3b004fb8962944fd3d1ce60101d0cbfec93
Rofeeq <rofeeqlawal@gmail.com>
2026-02-21 11:57:38 +0100

    New changes

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,7 +1,64 @@
-﻿namespace StudentProject.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentProject.DTOs;
+using StudentProject.Services.Interface;
+
+namespace StudentProject.Controllers
 {
-    public class StudentController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
     {
+        private readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await _studentService.GetAllStudentsAsync();
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudentById(int id)
+        {
+            var student = await _studentService.GetStudentByIdAsync(id);
+            if (student == null) return NotFound();
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent(CreateStudentRequest request)
+        {
+            try
+            {
+                var student = await _studentService.AddStudentAsync(request);
+                return CreatedAtAction(nameof(GetStudentById), new { id = student.Id }, student);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id, UpdateStudentRequest request)
+        {
+            var result = await _studentService.UpdateStudentAsync(id, request);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var result = await _studentService.DeleteStudentAsync(id);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,39 @@
+using StudentProject.Data;
+using StudentProject.Repositories;
+using StudentProject.Repositories.Interfaces;
+using StudentProject.Services;
+using StudentProject.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using System.Reflection.Metadata.Ecma335;
+using Scalar.AspNetCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddTransient<IStudentRepository, StudentRepository>();
+builder.Services.AddTransient<IStudentService, StudentService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
+
 
 app.UseHttpsRedirection();
 
